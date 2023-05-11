df1791cad326a183933e224861e6630f4bd1c012
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-05-11 23:25:06 +0300

    Add project files.

---
+++ Source/Diploma/DiplomaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DiplomaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIPLOMA_API ADiplomaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
