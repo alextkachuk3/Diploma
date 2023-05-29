96f3a3fd90ee8cfb9a7f2eaa9fb441946109dc83
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-05-29 04:14:29 +0300

    Spawn building on click

--- Source/Diploma/RTSController.cpp
+++ Source/Diploma/RTSController.cpp
@@ -1,32 +1,11 @@
 #include "RTSController.h"
 #include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include <Engine/ObjectLibrary.h>
-
-UInputAction* LoadInputActionByName(const FString& InputName)
-{
-	UObjectLibrary* ObjectLibrary = UObjectLibrary::CreateLibrary(UInputAction::StaticClass(), false, true);
-	ObjectLibrary->LoadAssetDataFromPath(TEXT("/Game/Input/Actions"));
-	ObjectLibrary->LoadAssetsFromAssetData();
-
-	TArray<FAssetData> AssetDataList;
-	ObjectLibrary->GetAssetDataList(AssetDataList);
-
-	for (const FAssetData& AssetData : AssetDataList)
-	{
-		if (AssetData.AssetName.ToString() == InputName)
-		{
-			UInputAction* InputAction = Cast<UInputAction>(AssetData.GetAsset());
-			if (InputAction)
-			{
-				return InputAction;
-			}
-		}
-	}
-
-	return nullptr;
-}
+#include "Landscape.h"
+#include "LandscapeComponent.h"
 
 ARTSController::ARTSController()
 {
@@ -43,40 +22,71 @@ void ARTSController::SetupInputComponent()
 	Subsystem->AddMappingContext(InputMappingContext, 0);
 
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
-	{		
-        EnhancedInputComponent->BindAction(LeftMouseClickAction, ETriggerEvent::Triggered, this, &ARTSController::OnInputStarted);
+	{
+		EnhancedInputComponent->BindAction(LeftMouseClickAction, ETriggerEvent::Triggered, this, &ARTSController::OnInputStarted);
 	}
 }
 
 void SpawnActorByName(UWorld* World, const FString& ActorClassName, const FVector& Location, const FRotator& Rotation)
 {
-    // Find the actor class by name
-    UClass* ActorClass = FindObject<UClass>(ANY_PACKAGE, *ActorClassName);
+	UClass* ActorClass = FindObject<UClass>(ANY_PACKAGE, *ActorClassName);
 
-    if (ActorClass)
-    {
-        // Spawn the actor
-        AActor* SpawnedActor = World->SpawnActor<AActor>(ActorClass, Location, Rotation);
-    }
-    else
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Failded to find actor: " + ActorClassName));
-    }
+	if (ActorClass)
+	{
+		AActor* SpawnedActor = World->SpawnActor<AActor>(ActorClass, Location, Rotation);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Failded to find actor: " + ActorClassName));
+	}
 }
 
 void ARTSController::BeginPlay()
 {
 	Super::BeginPlay();
 
-    UWorld* World = GetWorld();
-    FString ActorClassName = TEXT("Building");
-    FVector Location(-19787.0, 15018.7, 154.7);
-    FRotator Rotation(0.0f, 0.0f, 0.0f);
+	UWorld* World = GetWorld();
+	FString ActorClassName = TEXT("Building");
+	FVector Location(-19787.0, 15018.7, 154.7);
+	FRotator Rotation(0.0f, 0.0f, 0.0f);
 
-    SpawnActorByName(World, ActorClassName, Location, Rotation);
+	SpawnActorByName(World, ActorClassName, Location, Rotation);
 }
 
 void ARTSController::OnInputStarted()
 {
 	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Green, "Click");
+
+	FVector2D CursorPosition;
+	GetMousePosition(CursorPosition.X, CursorPosition.Y);
+	FVector WorldLocation, WorldDirection;
+
+	if (UGameplayStatics::DeprojectScreenToWorld(this, CursorPosition, WorldLocation, WorldDirection))
+	{
+		UWorld* World = GetWorld();
+		FString ActorClassName = TEXT("Building");
+		// WorldLocation.Z = 154;
+		// WorldDirection.Z = 154;
+		//FVector Location(WorldLocation);
+		FRotator Rotation(0.0f, 0.0f, 0.0f);
+
+		FHitResult HitResult;
+
+		ECollisionChannel TraceChannel = ECollisionChannel::ECC_WorldStatic;
+
+		GetWorld()->LineTraceSingleByChannel(HitResult, WorldLocation, WorldLocation + WorldDirection * 50000, TraceChannel);
+
+		/*TArray<AActor*> LandscapeActors;
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), ALandscape::StaticClass(), LandscapeActors);
+
+		ALandscape* Landscape = Cast<ALandscape>(LandscapeActors[0]);
+
+		FVector LocalLocation = Landscape->GetTransform().InverseTransformPosition(FVector(HitResult.Location.X, HitResult.Location.Y, 0.0f));*/
+
+
+		SpawnActorByName(World, ActorClassName, HitResult.Location, Rotation);
+
+		//SpawnActorByName(World, ActorClassName, LocalLocation, Rotation);
+
+	}
 }
